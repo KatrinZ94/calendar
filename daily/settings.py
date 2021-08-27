79dc3bc0de76b3b3b6b4b061c3630e664e875f59
KatrinZ94 <84146858+KatrinZ94@users.noreply.github.com>
2021-08-27 20:31:03 +0300

    Update settings.py

--- daily/settings.py
+++ daily/settings.py
@@ -145,8 +145,8 @@ AUTHENTICATION_BACKENDS = ['authentication.views.EmailBackend']
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'forallneedz@gmail.com'
-EMAIL_HOST_PASSWORD = 'arccos90'
+EMAIL_HOST_USER = '****'
+EMAIL_HOST_PASSWORD = '****'
 EMAIL_PORT = 587
 
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
